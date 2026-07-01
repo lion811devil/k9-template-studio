11ecf048498debd822e212a74cad3366c676b967
lion811devil <lion.811.devil.gn@gmail.com>
2026-07-01 19:25:32 +0200

    Aggiunge photo.js

---
+++ js/photo.js
@@ -0,0 +1,9 @@
+/*
+K9 Template Studio - photo.js
+Modulo foto soggetto: caricamento, opacità, dimensione, sfumatura bordi e rimozione sfondo chiaro.
+Nota: la logica attiva resta compatibile con editor.js finché il refactor sarà completato.
+*/
+window.K9PhotoModule = {
+  version: "1.0",
+  ready: true
+};
