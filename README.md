547dab5b2bf9189568709e5b28b9363ba167cf83
lion811devil <lion.811.devil.gn@gmail.com>
2026-07-02 21:20:45 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# K9 Template Studio V8.4
+
+Correzione V8.3:
+- riparata inizializzazione editor;
+- pannelli e tab funzionanti anche se una funzione avanzata non trova un elemento;
+- ordine script corretto;
+- cache PWA aggiornata;
+- versione visibile aggiornata.
+
+Caricare questi file nella root del repository sostituendo quelli esistenti.
---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "k9-template-studio-v8-4-cache-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/qrcode-local.js",
+  "./js/app.js",
+  "./js/editor.js",
+  "./js/archive.js",
+  "./assets/elements/elements.json",
+  "./assets/templates/templates.json",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
